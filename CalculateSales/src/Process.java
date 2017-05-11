b97bbf8134bd1eb5162588157f6e8fa84989154b
j_hotta <hotta.jun@m.alhinc.jp>
2017-05-11 17:38:19 +0900

    first commit

---
+++ CalculateSales/src/Process.java
@@ -0,0 +1,228 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
+public class Process {		
+	public static void main(String[] args){
+		//支店定義　例）001,北海道支店
+		HashMap<String, String> shop = new HashMap<String, String>();
+		//商品定義　例）SFT00001,Wandows
+		HashMap<String, String> item = new HashMap<String, String>();
+		
+		ArrayList<String> fileList = new ArrayList<String>();				//ディレクトリ内のファイルをすべて読み込む		
+		ArrayList<String> rcdList = new ArrayList<String>();				//.rcdが含まれるものだけのリスト		
+		ArrayList<Integer> fileSort = new ArrayList<Integer>();			//.rcdを除いたファイル名を数値として取得
+		ArrayList<String> renbanList = new ArrayList<String>();		 	//連番だけに絞る
+		
+		HashMap<String, Long> shopSum = new HashMap<String, Long>();		//店舗コード,　合計金額
+		HashMap<String, Long> itemSum = new HashMap<String,Long>();		//商品コード,　合計金額
+				
+		//支店定義ファイルを読み込む
+		File branch = new File("C:\\java\\branch.txt");	//args{0}+File.separator+branch.lst
+		BufferedReader branchBr = null;
+		try{
+			if(branch.exists() == true){
+				FileReader fl = new FileReader(branch);
+				branchBr = new BufferedReader(fl);
+				String s;	
+				while((s = branchBr.readLine()) != null){
+					String[] arr;
+					arr = s.split(",");	
+					int arrLength = arr[0].length();
+					boolean judge;
+					try{
+						Integer.parseInt(arr[0]);
+						judge = true;
+					}catch(NumberFormatException e){
+						judge = false;
+					}
+					if(arrLength == 3 && judge){							//支店コードが三桁かどうか、数値かどうか
+						if(arr.length == 2){								//支店名がカンマ、改行を含まない
+							shop.put(arr[0], arr[1]);
+							shopSum.put(arr[0],0L);
+						}else{
+							System.out.println("支店定義ファイルのフォーマットが不正です");
+						}
+					}else{
+						System.out.println("支店定義ファイルのフォーマットが不正です");
+					}
+				}		
+			}else{
+				System.out.println("支店定義ファイルが存在しません");
+				return;
+			}
+		}catch(IOException e){
+			System.out.println("予期せぬエラーが発生しました");
+			return;
+		}finally{
+			try{
+				branchBr.close();
+			}catch(IOException e){
+				System.out.println("予期せぬエラーが発生しました");
+				return;
+			}
+		}
+		
+		//商品定義ファイルを読み込む
+		File commodity = new File("C:\\java\\commodity.txt");	//args{0}+File.separator+commodity.lst
+		BufferedReader commodityBr = null;
+		try{
+			if(commodity.exists() == true){
+				FileReader fl = new FileReader(commodity);
+				commodityBr = new BufferedReader(fl);
+				String s;	
+				while((s = commodityBr.readLine()) != null){
+					String[] arr;
+					arr = s.split(",");	
+					int arrLeng = arr[0].length();
+					boolean judge;
+					try{
+						/////////////////////////////////////////
+					}catch(NumberFormatException e){
+						judge = false;
+					}
+					if(arrLeng == 8){									//商品コードがアルファベットと数字の八桁
+						if(arr.length == 2){							//商品名がカンマ、改行を含まない
+							item.put(arr[0], arr[1]);
+							itemSum.put(arr[0],0L);
+						}else{
+							System.out.println("商品定義ファイルのフォーマットが不正です");
+						}
+					}else{
+						System.out.println("商品定義ファイルのフォーマットが不正です");
+					}
+				}		
+			}else{
+				System.out.println("商品定義ファイルが存在しません");
+				return;
+			}
+		}catch(IOException e){
+			System.out.println("予期せぬエラーが発生しました");
+			return;
+		}finally{
+			try{
+				commodityBr.close();
+			}catch(IOException e){
+				System.out.println("予期せぬエラーが発生しました");
+				return;
+			}
+		}
+		
+		//売り上げファイルの読み込み
+		String filePath = "C:\\java\\";	//args{0}
+		File dir = new File(filePath);
+		String[] files = dir.list();
+		//ディレクトリ内のファイル名をString型で取得、ArrayList fileListに保管
+		for(int i = 0; i < files.length; i++){
+			fileList.add(files[i]);
+		}
+		//ArrayList fileList内のString型の変数の内、.rcdを含む文字列だけをArrayList rcdListに保管
+		int size = fileList.size();
+		String[] fileName = new String[size];
+		for(int i = 0; i < size; i++){
+			//例）fileName[0]にfileList(0)の文字列を代入
+			fileName[i] = fileList.get(i);
+			//fileName[0]に.rcdが含まれるならfileName[0]をrcdListに加える
+			if(fileName[i].contains(".rcd")){
+				rcdList.add(fileName[i]);				
+			}		
+		}
+		int rcdNum = rcdList.size();		
+		
+		//rcdListリストを読み込み、ファイル名と拡張子を区切り、数値に変換して新しいリストに格納
+		for(int i = 0; i < rcdNum; i++){
+			String splitRcd = rcdList.get(i);
+			String[] arr;
+			arr = splitRcd.split("\\.");
+			int henkan = Integer.parseInt(arr[0]); 
+			fileSort.add(henkan);
+		}
+		Collections.sort(fileSort);
+		int fileSortNum = fileSort.size();
+		
+		//連番になっている場合のみプログラムを通す
+		int j = fileSort.get(0);
+		for(int i = 0; i < fileSortNum; i++){
+			int num = fileSort.get(i);
+			String rcdRen = String.format("%08d",num);
+			String renban = String.format("%08d",j);
+			if(rcdRen.equals(renban)){
+				renbanList.add(rcdRen+".rcd");
+				j++;
+			}else{
+				System.out.println("売上ファイル名が連番になっていません");
+				break;
+			}	
+		}
+	
+		//集計
+		int renNum = renbanList.size();			
+		//売上ファイルを読み込む
+		for(int i = 0; i < renNum; i++){
+			try{
+				File file = new File("C:\\java\\"+renbanList.get(i));		//args{0}+File.separator + renbanList.get(i)
+				FileReader fr = new FileReader(file);
+				BufferedReader br = new BufferedReader(fr);
+				//一行目(支店コード)を読み込む
+				String first = br.readLine();
+				//二行目(商品コード)を読み込む
+				String second = br.readLine();
+				//三行目(売上金額)を読み込む
+				String third = br.readLine();
+				//三行目の数字の文字列をLong型の数値に変換
+				Long thirdNum = new Long(third);	
+				
+				//shopSum
+				long sumsrc1 = shopSum.get(first);
+				long bShopSum = sumsrc1 + thirdNum;	
+				shopSum.put(first, bShopSum);
+				
+				//itemSum
+				long sumsrc2 = itemSum.get(second);
+				long bItemSum = sumsrc2 + thirdNum;
+				itemSum.put(second,bItemSum);
+										
+				br.close();
+			}catch(IOException e){
+				System.out.println(e);
+			}				
+		}	
+		//支店別集計ファイル　出力
+		int shopNum = shop.size();
+		int itemNum = item.size();		
+		try{
+			File file = new File("C:\\java\\branchResult.txt"); //args{0}+File.separator+branch.out
+			FileWriter fw = new FileWriter(file);
+			BufferedWriter bw = new BufferedWriter(fw);
+			for(int i = 1; i <= shopNum; i++){
+				String format = String.format("%03d",i);
+				//long shopSumResult = shopSum.get(format);
+				bw.write(format+","+shop.get(format)+","+shopSum.get(format)+"\r\n");
+			}
+			bw.close();
+		}catch(IOException e){
+			System.out.println(e);
+		}	
+		//商品別集計ファイル　出力			
+		try{
+			File file = new File("C:\\java\\commodityResult.txt"); //args{0}+File.separator+commodity.out
+			FileWriter fw = new FileWriter(file);
+			BufferedWriter bw = new BufferedWriter(fw);
+			for(int i = 1; i <= itemNum; i++){
+				String format = String.format("SFT%05d",i);
+				bw.write(format+","+item.get(format)+","+itemSum.get(format)+"\r\n");		
+			}
+			bw.close();
+		}catch(IOException e){
+			System.out.println(e);
+		}finally{
+			//
+		}
+	}
+}
